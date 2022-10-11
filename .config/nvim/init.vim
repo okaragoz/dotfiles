0347736c6c8b987d07f3588bcb1c424802c29b83
Oguzcan Karagoz <oguzcankaragoz@gmail.com>
2022-10-11 23:28:17 +0200

    main

---
+++ .config/nvim/init.vim
@@ -0,0 +1,13 @@
+call plug#begin("~/.vim/plugged")
+  " Plugin Section
+
+Plug 'junegunn/vim-easy-align'
+Plug 'sheerun/vim-polyglot'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+Plug 'sainnhe/edge'
+
+call plug#end()
+"Config Section
+
+
+
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+qu=$SHELL
+ba=/bin/zsh
+
+if [ "$qu" == "$ba" ]
+    echo "Shell is ZSH"
+
+then
+ if [ ! -d "$HOME/.oh-my-zsh" ]; then
+      sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/okaragoz/dotfiles.git "$HOME/tmp"
+    cp -rf "$HOME/tmp/" "$HOME"
+    rm -rf "$HOME/tmp/"
+    echo "OhmyZSH and Dotfiles are installed"
+    else
+    git clone --depth=1 https://github.com/okaragoz/dotfiles.git "$HOME/tmp"
+    cp -rf "$HOME/tmp/" "$HOME"
+    rm -rf "$HOME/tmp/"
+    echo "Installing Personal Dotfiles for the first time"
+    fi
+else
+echo "Please install zsh shell first and run this script"
+fi  
